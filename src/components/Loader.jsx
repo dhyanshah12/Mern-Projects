e15bdcee7b4e36a4a24811e0e3e82878e393b3d1
dhyanshah12 <dhyanshah70@gmail.com>
2026-06-27 15:47:28 +0530

    loader css

---
+++ src/components/Loader.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Loader = () => {
+  return (
+    <>
+      <div class="loader">
+        <span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div class="base">
+          <span></span>
+          <div class="face"></div>
+        </div>
+      </div>
+      <div class="longfazers">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  )
+}
